5c8e0f315b0d4d5b48394a3e5509a62506506273
Evernote IT <evernoteit@casper-dev.rwc.etonreve.com>
2015-02-20 15:38:59 -0800

    added stop tomcat statements

--- upgrade_casper.sh
+++ upgrade_casper.sh
@@ -14,31 +14,60 @@ logger -p local7.info -t update_casper.sh "starting: $0 $*"
 test "$( whoami )" == "root" || \
     abort "Must run script as 'root'"
 
-if test `find /tmp/ROOT.war -mmin -1440`
+jss_zip=`find /tmp -name JSSInstallation*`
+filecount=`wc -l <<< "$jss_zip"`
+if [[ "$filecount" -lt 1 ]]; then
+    abort "JSS installation zip file not found"
+elif [[ "$filecount" -gt 1 ]]; then
+    abort "More than one JSS installation zip file found"
+fi
+
+if test `find "$jss_zip" -mmin -30`
 then
-    logger -p local7.info -t update_casper.sh "Up to date ROOT.war found"
-    echo "Up to date ROOT.war found"
+    logger -p local7.info -t update_casper.sh "Up to date JSS installation zip file found"
+    echo "Up to date JSS installation zip file found"
 else
-    abort "ROOT.war file missing or too old"
+    abort "JSS installation zip file older than 1 hour. Please download a new copy."
 fi
 
 ########## Main ##########
 
-#/etc/init.d/tomcat7 stop
-
-tomcat_alive=`ps -ef|grep tomcat |grep -v grep|wc -l`
-#test "$tomcat_alive" == 0 || \
-#    abort "Tomcat is still running"
-
-logger -p local7.info -t update_casper.sh "Backing up current database settings"
-cp /var/lib/tomcat7/webapps/ROOT/WEB-INF/xml/DataBase.xml /tmp
-
-logger -p local7.info -t update_casper.sh "Backing up current webapp files"
+# get the version of the existing JSS
 version=`grep -m1 \<version /var/lib/tomcat7/webapps/ROOT/WEB-INF/xml/version.xml| awk -F '[<|>]' '/version/{v=$3}{printf v}'`
 
-DIRECTORY=/var/lib/tomcat7/jss-$version
-if [ ! -d "$DIRECTORY" ]; then
-    mkdir $DIRECTORY
+# create the backup directory or replace it if it exists
+directory=/var/lib/tomcat7/jss-$version
+if [ ! -d "$directory" ]; then
+    mkdir $directory
 else
-    abort "Backup directory already exists"
+    echo "Backup directory exists. Replacing with new backup..."
+    rm -rf $directory
+    mkdir $directory
 fi
+
+# backup the database
+logger -p local7.info -t update_casper.sh "Backing up current database settings"
+cp /var/lib/tomcat7/webapps/ROOT/WEB-INF/xml/DataBase.xml $directory
+
+# backup the ROOT.war file
+logger -p local7.info -t update_casper.sh "Backing up current ROOT.war file"
+cp /var/lib/tomcat7/webapps/ROOT.war $directory
+
+# unzip the new jss installer
+#unzip "$jss_zip" -d "/tmp"
+
+# stop tomcat on both servers
+/etc/init.d/tomcat7 stop
+ssh galen@casperfe001 "sudo /etc/init.d/tomcat7 stop"
+
+# confirm tomcat is stopped, abort otherwise
+tomcat_alive=`ps -ef|grep tomcat |grep -v grep|wc -l`
+test "$tomcat_alive" == 0 || \
+    abort "Tomcat is still running on primary server"
+
+tomcat_alive2=`ssh galen@casperfe001 "ps -ef|grep tomcat |grep -v grep|wc -l"`
+test "$tomcat_alive2" == 0 || \
+    abort "Tomcat is still running on secondary server"
+
+# replace existing ROOT.war file
+
